9edafc81c0d74f06570526d23c1f01007f97ec40
christtina <christina.vhoward@gmail.com>
2017-11-11 14:05:49 -0500

    Integrating Charts to Application

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const app = electron.app;                     // Module to control application life.
+const app = electron.app; // Module to control application life.
 const BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
 const path = require('path');
 const url = require('url');
@@ -7,37 +7,37 @@ const ipcMain = require('electron').ipcMain;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var welcomeWindow;  // Application begins on this window.
+var welcomeWindow; // Application begins on this window.
 var mainWindow;
 
-function createMainWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({show: false});
-  mainWindow.maximize();
-  mainWindow.show();
+function createMainWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ show: false });
+    mainWindow.maximize();
+    mainWindow.show();
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'Views/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'Views/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 }
 
-function createWelcomeWindow () {
+function createWelcomeWindow() {
     // Create the browser window.
-    welcomeWindow = new BrowserWindow({show: false});
+    welcomeWindow = new BrowserWindow({ show: false });
     welcomeWindow.maximize();
     welcomeWindow.show();
 
@@ -52,7 +52,7 @@ function createWelcomeWindow () {
     // welcomeWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
-    welcomeWindow.on('closed', function () {
+    welcomeWindow.on('closed', function() {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
@@ -67,19 +67,19 @@ function createWelcomeWindow () {
 app.on('ready', createWelcomeWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 });
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 });
 
 
