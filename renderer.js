b92dba5a9c97584f5585595b3d424961cc690124
christtina <christina.vhoward@gmail.com>
2017-11-13 23:56:42 -0500

    Add data to chart & read executable console output

--- renderer.js
+++ renderer.js
@@ -8,19 +8,20 @@ const fs = require("fs");
 const path = require('path');
 const videojs = require('video.js');
 const Chart = require('chart.js');
+const readline = require('readline');
 
 function render_index() {
     ipcRenderer.send('render_index');
 }
 
 function setWorkspace() {
-    dialog.showOpenDialog({properties: ['openDirectory']}, function (dirName) {
+    dialog.showOpenDialog({ properties: ['openDirectory'] }, function(dirName) {
         localStorage.WORKSPACE = dirName;
     });
 }
 
 function fileExplorer() {
-    dialog.showOpenDialog(function (fileNames) {
+    dialog.showOpenDialog(function(fileNames) {
         if (fileNames === undefined) return;
 
         var fileName = fileNames[0];
@@ -39,50 +40,62 @@ function fileExplorer() {
     });
 }
 
+// adds data to the chart next to the video, initialized to have no data 
+function addData(chart, data) {
+    // chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+// creates a readline instance that we can use to read the output of the video
+// color segmentation video
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function execute() {
     const PROC = require('child_process').spawn('Executables/Larnx.exe', [document.getElementById("vid_original_header").getAttribute('data-video-path')]);
 
-    PROC.stdout.on('data', function(data)
-    {
+    PROC.stdout.on('data', function(data) {
         console.log(data);
     });
 
-    PROC.stderr.on("data", function (data)
-    {
+    PROC.stderr.on("data", function(data) {
         console.log(data);
     });
 
-    PROC.on('close', function (data)
-    {
+    PROC.on('close', function(data) {
         console.log(data);
     });
 
-    PROC.on('exit', function (data)
-    {
+    PROC.on('exit', function(data) {
         console.log(data);
     });
 }
 
 function saveFrame() {
-    const PROC = require('child_process').spawn('Executables/SaveFrame.exe',[ document.getElementById("vid_original_header").getAttribute('data-video-path') ]);
+    const PROC = require('child_process').spawn('Executables/SaveFrame.exe', [document.getElementById("vid_original_header").getAttribute('data-video-path')]);
 
-    PROC.stdout.on('data', function(data)
-    {});
+    PROC.stdout.on('data', function(data) {});
 
-    PROC.stderr.on("data", function (data)
-    {});
+    PROC.stderr.on("data", function(data) {});
 
-    PROC.on('close', function (data)
-    {});
+    PROC.on('close', function(data) {});
 
-    PROC.on('exit', function (data)
-    {});
+    PROC.on('exit', function(data) {});
 }
 
 
 
-
-
+// reads each line of the console input & adds the value to the PixelChart
+// TODO: move this block of code to where the executable that processes the color segmentation executable is called
+rl.question('', (y_data) => {
+    addData(myChart, y_data);
+    rl.close();
+});
 
 
 
