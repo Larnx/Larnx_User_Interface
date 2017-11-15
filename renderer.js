89917a655e8c1863962a3b1f6ebfa5cd386995ee
Kestutis Subacius <kestas@bu.edu>
2017-11-15 10:34:55 -0500

    Hooks to link UI to ContouMap process

--- renderer.js
+++ renderer.js
@@ -10,6 +10,9 @@ const videojs = require('video.js');
 const Chart = require('chart.js');
 const toastr = require('toastr');
 
+
+/* ALL USER INTERFACING CODE BELOW  */
+
 function render_index() {
     ipcRenderer.send('render_index');
 }
@@ -31,7 +34,13 @@ function render_modal(method){
                 '<br>' +
                 '                        <label for="timeStamp">Frame Time Location: </label>\n' +
                 '                        <input type="email" class="form-control" id="timeStamp" aria-describedby="timeStamp_descriptor" placeholder="default">\n' +
-                '                        <small id="timeStamp_descriptor" class="form-text text-muted">Please enter the video time in seconds where you would like to save a frame.</small>');
+                '                        <small id="timeStamp_descriptor" class="form-text text-muted">Please enter the video time in ms where you would like to save a frame.</small>');
+            break;
+        case 'trackContour':
+            $('#add_button_here').html('<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button> <button type="button" class="btn btn-primary" onclick="mapContour( document.getElementById(\'execute_fileName\').value )" data-dismiss="modal">Proceed</button>');
+            $('#add_forms_here').html('<label for="execute_fileName">Output File Name: </label>\n' +
+                '                        <input type="email" class="form-control" id="execute_fileName" aria-describedby="emailHelp" placeholder="default">\n' +
+                '                        <small id="emailHelp" class="form-text text-muted">Please enter the name you would like your processed outputs to be called.</small>');
             break;
         default:
     }
@@ -77,6 +86,8 @@ function pushDataPoint(y_value) {
     chart.options.data[0].dataPoints.push({ y: sum / y_value.length });
     chart.render();
 }
+
+/* ALL EXECUTABLE INTERFACING CODE BELOW */
 
 function execute(fileName) {
     const PROC = require('child_process').spawn('Executables/Larnx_Back_End.exe', [ '1' , document.getElementById("vid_original_header").getAttribute('data-video-path') , localStorage.WORKSPACE , fileName ]);
@@ -130,7 +141,32 @@ function saveFrame(fileName,timeStamp) {
     });
 }
 
+function mapContour(fileName) {
+    const PROC = require('child_process').spawn('Executables/Larnx_Back_End.exe', [ '4' , document.getElementById("vid_original_header").getAttribute('data-video-path') , localStorage.WORKSPACE , fileName ]);
+    //videoPlayer.play();
+    PROC.stdout.on('data', function(data)
+    {
+        console.log(data);
+        pushDataPoint(data);
+    });
 
+    PROC.stderr.on("data", function (data)
+    {
+        console.log(data);
+        pushDataPoint(data);
+    });
+
+    PROC.on('close', function (data)
+    {
+        console.log(data);
+    });
+
+    PROC.on('exit', function (data)
+    {
+        console.log(data);
+        toastr.success(path.join(localStorage.WORKSPACE,fileName),'Successful processing! Output is written to:')
+    });
+}
 
 
 
