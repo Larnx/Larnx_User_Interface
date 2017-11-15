c07bb7f8836df572856778faf555560921484849
Kestutis Subacius <kestas@bu.edu>
2017-11-14 22:17:31 -0500

    UI Improvments
    1) Modal added that is uniquly configured for any given C++ executable call. This modal allows user to select parameters
    2) Toastr based notifications added
    3) Plotting improved

--- renderer.js
+++ renderer.js
@@ -8,9 +8,34 @@ const fs = require("fs");
 const path = require('path');
 const videojs = require('video.js');
 const Chart = require('chart.js');
+const toastr = require('toastr');
 
 function render_index() {
     ipcRenderer.send('render_index');
+}
+
+function render_modal(method){
+    switch(method)
+    {
+        case 'execute':
+            $('#add_button_here').html('<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button> <button type="button" class="btn btn-primary" onclick="execute( document.getElementById(\'execute_fileName\').value )" data-dismiss="modal">Proceed</button>');
+            $('#add_forms_here').html('<label for="execute_fileName">Output File Name: </label>\n' +
+                '                        <input type="email" class="form-control" id="execute_fileName" aria-describedby="emailHelp" placeholder="default">\n' +
+                '                        <small id="emailHelp" class="form-text text-muted">Please enter the name you would like your processed outputs to be called.</small>');
+            break;
+        case 'saveFrame':
+            $('#add_button_here').html('<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button> <button type="button" class="btn btn-primary" onclick="saveFrame( document.getElementById(\'execute_fileName\').value , document.getElementById(\'timeStamp\').value)" data-dismiss="modal">Proceed</button>');
+            $('#add_forms_here').html('<label for="execute_fileName">Output File Name: </label>\n' +
+                '                        <input type="email" class="form-control" id="execute_fileName" aria-describedby="emailHelp" placeholder="default">\n' +
+                '                        <small id="emailHelp" class="form-text text-muted">Please enter the name you would like your processed outputs to be called.</small>\n' +
+                '<br>' +
+                '                        <label for="timeStamp">Frame Time Location: </label>\n' +
+                '                        <input type="email" class="form-control" id="timeStamp" aria-describedby="timeStamp_descriptor" placeholder="default">\n' +
+                '                        <small id="timeStamp_descriptor" class="form-text text-muted">Please enter the video time in seconds where you would like to save a frame.</small>');
+            break;
+        default:
+    }
+    $('#executeModal').modal('show');
 }
 
 function setWorkspace() {
@@ -53,10 +78,9 @@ function pushDataPoint(y_value) {
     chart.render();
 }
 
-
-function execute() {
-    const PROC = require('child_process').spawn('Executables/Larnx_Back_End.exe', [ '1' , document.getElementById("vid_original_header").getAttribute('data-video-path') , localStorage.WORKSPACE ]);
-
+function execute(fileName) {
+    const PROC = require('child_process').spawn('Executables/Larnx_Back_End.exe', [ '1' , document.getElementById("vid_original_header").getAttribute('data-video-path') , localStorage.WORKSPACE , fileName ]);
+    videoPlayer.play();
     PROC.stdout.on('data', function(data)
     {
         console.log(data);
@@ -77,11 +101,12 @@ function execute() {
     PROC.on('exit', function (data)
     {
         console.log(data);
+        toastr.success(path.join(localStorage.WORKSPACE,fileName),'Successful processing! Output is written to:')
     });
 }
 
-function saveFrame() {
-    const PROC = require('child_process').spawn('Executables/Larnx_Back_End.exe', [ '2' , document.getElementById("vid_original_header").getAttribute('data-video-path') , localStorage.WORKSPACE, '2500' ] );
+function saveFrame(fileName,timeStamp) {
+    const PROC = require('child_process').spawn('Executables/Larnx_Back_End.exe', [ '2' , document.getElementById("vid_original_header").getAttribute('data-video-path') , localStorage.WORKSPACE, timeStamp.toString() , fileName] );
 
     PROC.stdout.on('data', function(data)
     {
@@ -101,6 +126,7 @@ function saveFrame() {
     PROC.on('exit', function (data)
     {
         console.log(data);
+        toastr.success(path.join(localStorage.WORKSPACE,fileName),'Successful processing! Output is written to:')
     });
 }
 
